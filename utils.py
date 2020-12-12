72e0b1eeb32d850f8eef8f682f31bdd3bdce4a48
cx <nihao_cx@163.com>
2020-12-12 15:27:12 +0800

    feat(utils.py): 添加删除某单一分析文件夹函数

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from collections import Counter
+import os
+import shutil
 
 # 统计热力图不同热力值包含坐标数
 def countHotMap(fileName):
@@ -11,7 +13,20 @@ def countHotMap(fileName):
     for k, v in dict(Counter(l)).items():
         print('{} : {}'.format(k, v))
 
+# 删除某单一分析文件夹
+def deleteAnalysisDirList(analysisDirName):
+    tmpPath = '/home/cx/Desktop/sdb-dir/tmp'
+    for i in range(1, 42):
+        fileName = '30.113.151.{}/{}'.format(i, analysisDirName)
+        filePath = os.path.join(tmpPath, fileName)
+        if os.path.isdir(filePath):
+            print(filePath)
+            # shutil.rmtree(filePath)
+    
+
 
 if __name__ == '__main__':
-    fileName = '/home/cx/Desktop/sdb-dir/tmp/30.113.151.1/analysisHandover/WLAN1漫游热力图统计数据.csv'
-    countHotMap(fileName)
+    # fileName = '/home/cx/Desktop/sdb-dir/tmp/30.113.151.1/analysisHandover/WLAN1漫游热力图统计数据.csv'
+    # countHotMap(fileName)
+
+    # deleteAnalysisDirList('analysisRtt')
